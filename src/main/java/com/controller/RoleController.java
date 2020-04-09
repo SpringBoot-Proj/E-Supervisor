86a1c7224b0fc1d1e3b78303f7fab9132f861ada
swapnil15dave <swapnil15dave@gmail.com>
2020-04-10 00:00:35 +0530

    listRoles api updated in RoleController

--- src/main/java/com/controller/RoleController.java
+++ src/main/java/com/controller/RoleController.java
@@ -1,13 +1,17 @@
 package com.controller;
 
+import java.util.ArrayList;
+
 import org.hibernate.validator.constraints.SafeHtml.Attribute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bean.ResponseBean;
+import com.bean.RoleBean;
 import com.bean.UserBean;
 import com.dao.RoleDao;
 
@@ -41,4 +45,25 @@ public class RoleController {
 		return responseBean;
 	}
 	
+	@GetMapping("/listRoles")
+	
+	public ResponseBean<ArrayList<RoleBean>> getRoles()
+	{
+		ArrayList<RoleBean> roles = roleDao.listRoles();
+		ResponseBean<ArrayList<RoleBean>> responseBean = new ResponseBean<>();
+		
+		if(roles!=null)
+		{	responseBean.setData(roles);
+			responseBean.setCode(200);
+			responseBean.setMessage(roles.size() + " number of roles");
+			
+		}
+		else
+		{
+			responseBean.setCode(400);
+			responseBean.setMessage("no such record");
+			responseBean.setData(null);
+		}
+		return (ResponseBean<ArrayList<RoleBean>>) responseBean;
+	}
 }
