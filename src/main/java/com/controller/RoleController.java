fd9a0438842d6532182f7742f86cf9f6bec77408
swapnil15dave <swapnil15dave@gmail.com>
2020-04-10 10:26:38 +0530

    modification in role controller

--- src/main/java/com/controller/RoleController.java
+++ src/main/java/com/controller/RoleController.java
@@ -2,12 +2,9 @@ package com.controller;
 
 import java.util.ArrayList;
 
-import org.hibernate.validator.constraints.SafeHtml.Attribute;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bean.ResponseBean;
