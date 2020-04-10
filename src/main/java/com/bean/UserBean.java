dad79d90440f4b2745bcadfbe2bc38f94f606bd7
Aastha112 <63099576+Aastha112@users.noreply.github.com>
2020-04-10 14:27:54 +0530

    added conflicted files

--- src/main/java/com/bean/UserBean.java
+++ src/main/java/com/bean/UserBean.java
@@ -2,18 +2,27 @@ package com.bean;
 
 public class UserBean {
 
+<<<<<<< HEAD
 	private int user_id;
 	private String first_name;
 	private String last_name;
 	private String email;
 	private String password;
 	
+=======
+	int user_id;
+	String first_name, last_name, email, password;
+>>>>>>> 23551f7... added user bean
 	public int getUser_id() {
 		return user_id;
 	}
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
+<<<<<<< HEAD
+=======
+	
+>>>>>>> 23551f7... added user bean
 	public String getFirst_name() {
 		return first_name;
 	}
@@ -38,6 +47,7 @@ public class UserBean {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+<<<<<<< HEAD
 	@Override
 	public String toString() {
 		return "UserBean [user_id=" + user_id + ", first_name=" + first_name + ", last_name=" + last_name + ", email="
@@ -46,4 +56,9 @@ public class UserBean {
 	
 	
 
+=======
+	
+	
+	
+>>>>>>> 23551f7... added user bean
 }
