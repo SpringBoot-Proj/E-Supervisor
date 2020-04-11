d3ffd63bcc95fd8a93c66ea698eeb96989c0b26b
Aastha112 <63099576+Aastha112@users.noreply.github.com>
2020-04-11 08:46:06 +0530

    modified user bean

--- src/main/java/com/bean/UserBean.java
+++ src/main/java/com/bean/UserBean.java
@@ -1,28 +1,9 @@
 package com.bean;
 
-public class UserBean {
-
-<<<<<<< HEAD
+public class UserBean
+{
+	private String first_name,last_name,email,password;
 	private int user_id;
-	private String first_name;
-	private String last_name;
-	private String email;
-	private String password;
-	
-=======
-	int user_id;
-	String first_name, last_name, email, password;
->>>>>>> 23551f7... added user bean
-	public int getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
-<<<<<<< HEAD
-=======
-	
->>>>>>> 23551f7... added user bean
 	public String getFirst_name() {
 		return first_name;
 	}
@@ -47,18 +28,13 @@ public class UserBean {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-<<<<<<< HEAD
-	@Override
-	public String toString() {
-		return "UserBean [user_id=" + user_id + ", first_name=" + first_name + ", last_name=" + last_name + ", email="
-				+ email + ", password=" + password + "]";
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
 	}
 	
 	
-
-=======
 	
-	
-	
->>>>>>> 23551f7... added user bean
 }
