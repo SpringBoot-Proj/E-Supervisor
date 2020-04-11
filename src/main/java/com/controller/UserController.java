2997a5ccf5251895bb34777bb8b2d1d121c707bd
Aastha112 <63099576+Aastha112@users.noreply.github.com>
2020-04-11 08:46:47 +0530

    modified user bean

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bean.ResponseBean;
 import com.bean.UserBean;
 import com.dao.UserDao;
 
@@ -15,9 +16,15 @@ public class UserController {
 	UserDao userDao;
 	
 	@PostMapping("/adduser")
-	public void add_user(UserBean userBean,String roleName)
+	public ResponseBean<UserBean> add_user(UserBean userBean,String roleName)
 	{
-		userDao.add_user(userBean);
+		int userid = userDao.add_user(userBean);
+	
+		ResponseBean<UserBean> responseBean = new ResponseBean<>();
+		responseBean.setMessage("user added");
+		responseBean.setCode(200);
+		return responseBean;
+	
 	}
 	
 	@PutMapping("/updatepro")
