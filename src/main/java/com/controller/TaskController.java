89851fbd3cdb6852bbb56e683bec91fb9d705999
unknown <sanketp1711@gmail.com>
2020-04-15 10:42:13 +0530

    add_task

--- src/main/java/com/controller/TaskController.java
+++ src/main/java/com/controller/TaskController.java
@@ -1,12 +1,19 @@
 package com.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bean.ResponseBean;
+import com.bean.TaskBean;
 import com.bean.UserBean;
 import com.dao.TaskDao;
 
@@ -33,5 +40,137 @@ public class TaskController {
 			responseBean.setMessage("Success");
 		}
 		return responseBean;
+	}
+	@PostMapping("/add_task")
+	public ResponseBean<TaskBean> addTask(TaskBean taskBean)
+	{
+		taskDao.addTask(taskBean);
+		ResponseBean<TaskBean> rb = new ResponseBean<>();
+		rb.setMessage("task added");
+		rb.setCode(200);
+		rb.setData(taskBean);
+		
+		return rb;
+	}
+	
+	@PostMapping("/update_task/{task_id}")
+	public ResponseBean<TaskBean> updateTask(TaskBean taskBean,@PathVariable("task_id") int task_id)
+	{
+		TaskBean tb=taskDao.updateTask(taskBean,task_id);
+		ResponseBean<TaskBean> rb = new ResponseBean<>();
+		
+		if(tb!=null)
+		{
+			rb.setCode(200);
+			rb.setMessage("Task updated.");
+			rb.setData(tb);
+		}
+		else {
+			rb.setCode(400);
+			rb.setMessage("No task found");
+			rb.setData(null);
+		}
+		return rb;
+	}
+	
+	@DeleteMapping("delete_task/{task_id}")
+	public ResponseBean<TaskBean> deleteTask(@PathVariable("task_id") int task_id)
+	{
+		boolean b=taskDao.deteleTask(task_id);
+		
+		ResponseBean rb = new ResponseBean();
+		
+		if(b)
+		{
+			rb.setCode(200);
+			rb.setMessage("Task deleted.");
+			rb.setData(1);
+		}
+		else {
+			rb.setCode(400);
+			rb.setMessage("No task found");
+			rb.setData(0);
+		}
+		return rb;
+		
+	}
+	@GetMapping("/list_tasks")
+	public ResponseBean<ArrayList<TaskBean>> listTasks() {
+		
+		ArrayList<TaskBean> tasks = taskDao.listTasks();
+		ResponseBean<ArrayList<TaskBean>> rb = new ResponseBean<>();
+		
+		if(tasks!=null)
+		{	rb.setData(tasks);
+			rb.setCode(200);
+			rb.setMessage(tasks.size()+" tasks found.");
+			
+		}
+		else
+		{
+			rb.setCode(400);
+			rb.setMessage("No tasks found.");
+		}
+		return rb;
+	}
+	
+	@GetMapping("/view_task/{task_id}")
+	public ResponseBean<TaskBean> viewTask(@PathVariable("task_id") int task_id) {
+		TaskBean taskBean = taskDao.viewTask(task_id);
+		ResponseBean<TaskBean> rb = new ResponseBean<>();
+		
+		if(taskBean!=null)
+		{
+			rb.setCode(200);
+			rb.setMessage("Task found");
+			rb.setData(taskBean);
+		}
+		else
+		{
+			rb.setCode(400);
+			rb.setMessage("No task found");
+		}
+		return rb;
+		
+	}
+	
+	@PostMapping("/complete_task/{task_id}")
+	public ResponseBean<TaskBean> completeTask(@PathVariable("task_id") int task_id)
+	{
+		TaskBean taskBean=taskDao.completeTask(task_id);
+		
+		ResponseBean<TaskBean> rb = new ResponseBean<>();
+		
+		if(taskBean!=null)
+		{
+			rb.setCode(200);
+			rb.setMessage("Task is marked as completed.");
+			rb.setData(taskBean);
+		}
+		else {
+			rb.setCode(400);
+			rb.setMessage("No task found");
+			rb.setData(null);
+		}
+		return rb;
+	}
+	
+	@PostMapping("/reopen_task/{task_id}")
+	public ResponseBean<TaskBean> reopenTask(@PathVariable("task_id") int task_id, TaskBean taskBean)
+	{
+		ResponseBean<TaskBean> rb = new ResponseBean<>();
+		TaskBean tb=taskDao.reopenTask(task_id,taskBean);
+		if(tb!=null)
+		{
+			rb.setCode(200);
+			rb.setMessage("Task is reopened and marked as incomplete");
+			rb.setData(tb);
+		}
+		else {
+			rb.setCode(400);
+			rb.setMessage("No task found");
+			rb.setData(null);
+		}
+		return rb;
 	}
 }
--- src/main/java/com/dao/TaskDao.java
+++ src/main/java/com/dao/TaskDao.java
@@ -2,16 +2,20 @@ package com.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.bean.RoleBean;
+import com.bean.TaskBean;
 import com.bean.UserBean;
 
 @Repository
@@ -55,6 +59,94 @@ public class TaskDao {
 		RoleBean roleBean = stmt.queryForObject(sql, new BeanPropertyRowMapper<RoleBean>(RoleBean.class));
 		System.out.println("Role 2 "+roleBean.getRoleName());
 		return roleBean!=null?roleBean.getRoleID():-1;
+	}
+
+	public void addTask(TaskBean taskBean) {
+		
+		stmt.update("insert into task (task_name,description,start_date,end_date,user_id,admin_id) values (?,?,?,?,?,?)",taskBean.getTask_name(),taskBean.getDescription(),taskBean.getStart_date(),taskBean.getEnd_date(),taskBean.getUser_id(),taskBean.getAdmin_id());
+		
+	}
+
+	public TaskBean updateTask(TaskBean taskBean, int task_id) {
+		
+		System.out.println(taskBean.getEnd_date());
+		int i=stmt.update("update task set task_name=?, description=?, start_date=?, end_date=?, user_id=?, admin_id=? where task_id=?",taskBean.getTask_name(),taskBean.getDescription(),taskBean.getStart_date(),taskBean.getEnd_date(),taskBean.getUser_id(),taskBean.getAdmin_id(),task_id);
+		if(i==1)
+			return viewTask(task_id);
+		else
+			return null;
+	}
+
+	public boolean deteleTask(int task_id) {
+		
+		int i=stmt.update("delete from task where task_id="+task_id);
+		if(i==1)
+			return true;
+		else
+			return false;
+	}
+	
+	public ArrayList<TaskBean> listTasks() {
+
+		ArrayList<TaskBean> list = (ArrayList<TaskBean>) stmt.query("select * from task", new Rm());
+		return list;
+
+	}
+
+	class Rm implements RowMapper<TaskBean> {
+
+		public TaskBean mapRow(ResultSet rs, int rowNum) throws SQLException {
+			TaskBean bean = new TaskBean();
+			
+			bean.setUser_id(rs.getInt("user_id"));
+			bean.setTask_id(rs.getInt("task_id"));
+			bean.setTask_name(rs.getString("task_name"));
+			bean.setDescription(rs.getString("description"));
+			bean.setStart_date(rs.getString("start_date"));
+			bean.setEnd_date(rs.getString("end_date"));
+			bean.setIsComplete(rs.getInt("iscomplete"));
+			bean.setCompletion_date(rs.getString("completion_date"));
+			bean.setComment(rs.getString("comment"));
+			bean.setAdmin_id(rs.getInt("admin_id"));
+			
+			
+			return bean;
+		}
+
+	}
+
+	public TaskBean viewTask(int task_id) {
+		//Try Catch because queryForObject return EmptyResultDataAccessException if it does not found record
+		try {
+			TaskBean task = stmt.queryForObject("select * from task where task_id  ="+ task_id +"",new BeanPropertyRowMapper<TaskBean>(TaskBean.class));
+			return task;
+		}
+		catch(EmptyResultDataAccessException e)
+		{
+			return null;
+		}
+	}
+
+	public TaskBean completeTask(int task_id) {
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = new Date();
+		String date_time = formatter.format(date);
+		System.out.println(date_time);
+		int i=stmt.update("update task set iscomplete=1,comment='',completion_date=? where task_id=?",date_time,task_id);
+		if(i==1)
+			return viewTask(task_id);
+		else
+			return null;
+	}
+
+	public TaskBean reopenTask(int task_id, TaskBean taskBean) {
+		
+		int i=stmt.update("update task set iscomplete=0, completion_date='', comment=? where task_id=? ",taskBean.getComment(),task_id);
+		if(i==1)
+			return viewTask(task_id);
+		else
+			return null;
 	}
 
 }
