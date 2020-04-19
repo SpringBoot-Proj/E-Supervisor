9b3b5c39d97a5a646f6224c82853fef504911269
ksjadeja <kuldeepjadeja459@gmail.com>
2020-04-19 20:45:05 +0530

    files added

---
+++ src/main/java/com/bean/UserBean.java
@@ -0,0 +1,49 @@
+package com.bean;
+
+public class UserBean {
+
+	private int user_id;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private String password;
+	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "[user_id=" + user_id + ", first_name=" + first_name + ", last_name=" + last_name + ", email="
+				+ email + ", password=" + password + "]";
+	}
+	
+	
+
+}
